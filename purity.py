9c4329fdc1bf5e85b2fb8fcb2e27bde8c415b704
joaoflauzino <joaolucas.apoio@gmail.com>
2020-09-30 18:28:51 -0300

    Final version

--- purity.py
+++ purity.py
@@ -2,34 +2,58 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
-matplotlib.use('Agg')
 from sklearn.cluster import KMeans
 from sklearn import datasets
+import numpy as np
+from sklearn import metrics
+import seaborn as sns
 
 
-def visualize_model_results(x, labels):
-    plt.scatter(x.Petal_Length, x.Petal_width,c=colormap[labels],s=40)
-    plt.title('Classification K-means ')
-    plt.show()
+def visualize(df):
+    selectors = ['variety', 'pred']
+    for element in range(len(selectors)):
+        s = sns.scatterplot(data=df, x="petal.length", y="petal.width", hue=df[selectors[element]].tolist())
+        fig = s.get_figure()
+        fig.savefig('image/' + selectors[element] + '.png')
+        fig.clf()
+
+def apply_model(x):
+    Model = KMeans(n_clusters=3)
+    Model.fit(x)
+    return Model
+
+def select(x):
+    if x == 'Setosa':
+        return '0'
+    if x == 'Versicolor':
+        return '1'
+    else:
+        return '2'
+
+def purity(df):
+    df['pred'].apply(lambda x: select(x))
+    confusion_matrix = metrics.cluster.contingency_matrix(df['variety'], df['pred'])
+    return np.sum(np.amax(confusion_matrix, axis=0)) / np.sum(confusion_matrix) 
 
 if __name__ == "__main__":
-
-    colormap=np.array(['Red','green','blue'])
-
-    iris = datasets.load_iris()
-    x = pd.DataFrame(iris.data)
-    x.columns = ['Sepal_Length','Sepal_width','Petal_Length','Petal_width']
-    y = pd.DataFrame(iris.target)
-    y.columns = ['Targets']
-
-    Model = KMeans(n_clusters=3)
-    # Apply model
-    Model.fit(x)
     
-    #Visualize results
-    visualize_model_results(x, Model.labels_)
+    df = pd.read_csv('dataset/iris.csv', sep = ",")
+    x = df[['sepal.length','sepal.width','petal.length','petal.width']]
+    y = df[['variety']]
 
-    #print(Model.labels_)
+    # Apply model
+    Model = apply_model(x)
+
+    # Save result in a column
+    df['pred'] = Model.labels_
+
+    # Save results in png file
+    visualize(df)
+    
+    # Calculate purity
+    purity_value = purity(df)
+
+    print('The purity value is: ', purity_value)
 
 
 
