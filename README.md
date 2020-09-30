841005f7d036af251268fb1e1af0bb0a9b590d48
joaoflauzino <joaolucas.apoio@gmail.com>
2020-09-30 20:26:18 -0300

    Final Version

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # purity
+
+# KMeans_Implementation
+
+- Para executar o código é necessário executar o seguinte comando:
+
+    - python purity.py
+
+- O código irá salvar imagens dos agrupamentos na pasta image
+
+- É necessário que tenha as bibliotecas sklearn, pandas, seaborn instaladas no ambiente, caso não tenha, instalar utilizando os seguintes comandos no prompt:
+
+    - pip install sklearn
+    - pip install pandas
+    - pip install seaborn
--- purity.py
+++ purity.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
-import numpy as np
 from sklearn import metrics
 import seaborn as sns
+from collections import Counter
 
 # Function to visualize groups
-def visualize(df, n_cluster):
-    s = sns.scatterplot(data=df, x="petal.length", y="petal.width", hue=df['pred'].tolist())
+def visualize(df, Model, n_cluster):
+    s = sns.scatterplot(data=df, x="petal.length", y="petal.width", hue=Model)
     fig = s.get_figure()
     fig.savefig('image/kmeans_clusters_{}.png'.format(n_cluster))
     fig.clf()
@@ -15,14 +15,18 @@ def visualize(df, n_cluster):
 # Function to apply kmeans
 def apply_model(x, k):
     Model = KMeans(k)
-    Model.fit(x)
-    return Model
+    return Model.fit_predict(x)
 
 # Function to calculate purity
-def purity(df):
-    confusion_matrix = metrics.cluster.contingency_matrix(df['variety'], df['pred'])
-    # Taking the sum of the ratings and dividing by the total
-    return np.sum(np.amax(confusion_matrix, axis=0)) / np.sum(confusion_matrix) 
+def purity_metric(target, prediction):
+   
+    majority_sum = 0  
+    for cl in set(prediction):
+        labels_cl = Counter(l for l, c in zip(target, prediction) if c == cl)
+        majority_sum += max(labels_cl.values())
+
+    return majority_sum / len(prediction)
+
 
 if __name__ == "__main__":
     
@@ -33,18 +37,11 @@ if __name__ == "__main__":
     for n_cluster in [2,3,4]:
         # Apply model
         Model = apply_model(x, n_cluster)
-
-        import pdb; pdb.set_trace()
-
-        # Saving result model in a column
-        df['pred'] = Model.labels_
-
         # Saving results in png file
-        visualize(df, n_cluster)
-    
+        visualize(df, Model, n_cluster)
         # Calculating purity
-        purity_value = purity(df)
-
+        purity_value = purity_metric(df['variety'], Model)
+   
         print('The purity value with {} clusters is: '.format(str(n_cluster)), purity_value)
 
 
